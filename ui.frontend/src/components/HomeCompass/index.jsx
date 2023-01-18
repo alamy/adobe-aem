6725f3765325f34d45a079c1b44708980d6f4b0f
alamy <alamy.nt@gmail.com>
2023-01-18 20:03:05 -0300

    Construção da Home

--- ui.frontend/src/components/HomeCompass/index.jsx
+++ ui.frontend/src/components/HomeCompass/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Validar from  "../FormCompass/validar.json";
+import "./style.css";
 //import  Estados from'./estados.json';
 //import  Municipios from'./municipios.json';
 
@@ -64,12 +65,12 @@ const HomeCompass = ({
     let user = sessionStorage.getItem('user');
     let pass = sessionStorage.getItem('pass');
     if(!user || !pass){
-        window.location.replace("http://localhost:4502/content/reactapp/us/en/login.html");
+      //  window.location.replace("http://localhost:4502/content/reactapp/us/en/login.html");
     }else{
         for(let i = 0; i < objeto.length; i++){
                 if(user !== objeto[i].name || pass !== objeto[i].senha){
                     console.log("vc não deveria está aqui")
-                    window.location.replace("http://localhost:4502/content/reactapp/us/en/login.html");
+               //     window.location.replace("http://localhost:4502/content/reactapp/us/en/login.html");
                 }else {
                     console.log("seja muito bemvindo " + x)
                     getLocation()
@@ -109,22 +110,40 @@ const HomeCompass = ({
     if(!logoHome) {
         logo = '/content/dam/espatalho.png';
     }else{
-        logo = logoHome
+        logo = logoHome.src
     }
-    console.log(itemReactTitle)
-    console.log(itemEAMParagrafo)
     return (
         <>
-            <img src={logo} alt="logo" />
-            <h1>{itemReactTitle.title1}</h1>
-            <h1>{itemReactTitle.title2}</h1>
-            <h1>{itemReactTitle.title3}</h1>
-            <h1>{itemReactTitle.title4}</h1>
-            <p>{itemReactParagrafo.paragrafo1}</p>
-            <p>{itemReactParagrafo.paragrafo2}</p>
-            <p>{itemReactParagrafo.paragrafo3}</p>
-            <p>{itemReactParagrafo.paragrafo4}</p>
-            <p>{textoFotter}</p>
+        <div class="bodyHome">
+            <ul class="header">
+                <li class="logoHome">
+                    <img src={logo} alt="logo" />
+                </li>
+                <li class="hora">
+                    <h1>Hora</h1>
+                    <p>Data asdasd</p>
+                </li>
+                <li class="localidade">
+                    <p>Local</p>
+                    <h2>Temperatura</h2>
+                </li>
+            </ul>
+                
+            <section class="bodyCompass">
+                <h3>{titleOne}</h3>
+                <p>{desciptionOne}</p>
+                <h2>{titleTwo}</h2>
+                <p>{desciptionTwo}</p>
+                <h1>{titleThree}</h1>
+                <p>{desciptionThree}</p>
+                <h1>{titleFour}</h1>
+                <p>{desciptionFour}</p>
+            </section>    
+
+            <section class="footerHome">
+                <p>{textoFotter}</p>
+            </section>
+        </div>
         </>
     )
 }
